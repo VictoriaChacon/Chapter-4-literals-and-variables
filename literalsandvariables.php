b6788d73b36816b90d6e134e2270c3585693dc60
Victoria Chacon <Vchaco06@gmail.com>
2016-06-20 22:28:11 -0600

    Finally got to work with the different literals and variables.

---
+++ literalsandvariables.php
@@ -0,0 +1,11 @@
+<?php
+$myname = "Victoria";
+$myage = "24";
+
+echo "a: " . 74 . "<br>";
+echo "b: " . "I am coding in PHP" . "<br>";
+echo "c: " . FALSE . "<br>";
+echo "d: " . $myname . "<br>";
+echo "e: " . $myage . "<br>";
+echo "f: " . TRUE . "<br>";
+?>
